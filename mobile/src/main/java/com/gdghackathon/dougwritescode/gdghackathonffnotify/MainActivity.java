64b23cf217c558044fcaf74d83988c1ba65b33b3
matrixrunner <dougwritescode@gmail.com>
2014-11-01 14:41:01 -0600

    Updated data retrieval for weather, started on footballer data grabbing.

--- mobile/src/main/java/com/gdghackathon/dougwritescode/gdghackathonffnotify/MainActivity.java
+++ mobile/src/main/java/com/gdghackathon/dougwritescode/gdghackathonffnotify/MainActivity.java
@@ -4,24 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    static final String PLAYERS = "saved players";
+    WeatherGrabber weathergrabber = new WeatherGrabber();
+    FFDataGrabber ffdatagrabber = new FFDataGrabber();
+    ArrayList<String> savedplayers = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.savedplayers = savedInstanceState.getStringArrayList(PLAYERS);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // getMenuInflater().inflate(R.menu.menu_main, menu); // for now
         return true;
     }
-
+    /*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -35,5 +42,12 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    */ // for now
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putStringArrayList(PLAYERS, savedplayers);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
